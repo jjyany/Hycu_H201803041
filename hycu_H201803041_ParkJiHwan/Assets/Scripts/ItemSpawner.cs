4d9821ddbb90ae00e07b7f2606f1fadf4dcf2051
jihwanang <ji.hwanang@gmail.com>
2022-11-28 18:36:10 +0900

    (UI)스크립트

---
+++ hycu_H201803041_ParkJiHwan/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject[] items;          //생성될 아이템(프리펩) 오브젝트 배열
+    public Transform playerTransform;   //생성될 반경의 위치
+    
+    private float lastSpawnTime;        //마지막에 아이템이 생성된 시간
+    public float maxDistance = 5f;      //플레이어를 중심으로 최대반경
+    
+    private float timeBetSpawn;         //아이템 생성후 다음생성까지 소요될 대기시간
+
+    public float timeBetSpawnMax = 7f;  //스폰 대기시간의 최대 대기시간
+    public float timeBetSpawnMin = 2f;  //스폰 대기시간의 최소 대기시간
+
+    private void Start()
+    {
+        timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+        lastSpawnTime = 0f;
+    }
+
+    private void Update()
+    {
+        if(Time.time >= lastSpawnTime + timeBetSpawn && playerTransform != null)
+        {
+            Spawn();
+            lastSpawnTime = Time.time;
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+        }
+    }
+
+    private void Spawn()
+    {
+        //(플레이어중심, 최대반경, 맵의 모든 부분)
+        var spawnPosition = Utility.GetRandomPointOnNavMesh(playerTransform.position, maxDistance, NavMesh.AllAreas);
+
+        //생성되는 위치값 수정
+        spawnPosition += Vector3.up * 0.5f;
+
+        //item에 Instantiate(items배열의 길이만큼 랜덤의 Index값으로 생성, 생성될 위치값, 회전값)
+        var item = Instantiate(items[Random.Range(0, items.Length)], spawnPosition, Quaternion.identity);
+
+        //생성된 아이템은 5초뒤에 삭제
+        Destroy(item, 5f);
+    }
+}
---
+++ hycu_H201803041_ParkJiHwan/Assets/Scripts/UIManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    
+    public static UIManager Instance
+    {
+        //UIManager 스크립트를 싱클톤으로 구현(다른 스크립트가 쉽게 접근할수 있도록)
+        get //접근
+        {
+            //UIManager가 할당되지 않았다면 강제로 할당
+            if (instance == null) instance = FindObjectOfType<UIManager>();
+
+            return instance;
+        }
+    }
+
+    //외부에서 접근할 필요가 없기에 private로 선언
+    [SerializeField] private GameObject gameoverUI;
+
+    //크로스헤어는 별도로 관리(2가지 조준점을 표시해야하기 때문)
+    [SerializeField] private Crosshair crosshair;
+
+    [SerializeField] private Text healthText;
+    [SerializeField] private Text lifeText;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text ammoText;
+    [SerializeField] private Text waveText;
+
+    public void UpdateAmmoText(int magAmmo, int remainAmmo)
+    {
+        ammoText.text = magAmmo + "/" + remainAmmo;
+    }
+
+    public void UpdateScoreText(int newScore)
+    {
+        scoreText.text = "Score : " + newScore;
+    }
+    
+    public void UpdateWaveText(int waves, int count)
+    {
+        waveText.text = "Wave : " + waves + "\nEnemy Left : " + count;
+    }
+
+    public void UpdateLifeText(int count)
+    {
+        lifeText.text = "Life : " + count;
+    }
+
+    public void UpdateCrossHairPosition(Vector3 worldPosition)
+    {
+        crosshair.UpdatePosition(worldPosition);
+    }
+    
+    public void UpdateHealthText(float health)
+    {
+        healthText.text = Mathf.Floor(health).ToString();
+    }
+    
+    public void SetActiveCrosshair(bool active)
+    {
+        crosshair.SetActiveCrosshair(active);
+    }
+    
+    public void SetActiveGameoverUI(bool active)
+    {
+        gameoverUI.SetActive(active);
+    }
+    
+    public void GameRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
